7de66231a00cdfdf3f4ef957b6f8878c72e17259
ash214-an <intimacy222@naver.com>
2025-08-15 16:49:08 +0900

    first commit

---
+++ Source/SpartaProject2/Private/Item.cpp
@@ -0,0 +1,30 @@
+
+
+#include "Item.h"
+
+AItem::AItem()
+{
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Game/Resources/Props/SM_Chair.SM_Chair"));
+	if (MeshAsset.Succeeded())
+	{
+		StaticMeshComp->SetStaticMesh(MeshAsset.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UMaterial> MaterialAsset(TEXT("/Game/Resources/Materials/M_Metal_Gold.M_Metal_Gold"));
+	if (MaterialAsset.Succeeded())
+	{
+		StaticMeshComp->SetMaterial(0, MaterialAsset.Object);
+	}
+}
+
+	
+
+
+
+
